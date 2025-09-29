5795f38950ad1f910ae4564232327bafc16cf5c9
Szabolcs József Péter <Szabolcs-Jozsef.Peter@t-systems.com>
2025-09-29 11:59:24 +0200

    Lifecycle methods

--- Calculator/test/com/appsdeveloperblog/CalculatorTest.java
+++ Calculator/test/com/appsdeveloperblog/CalculatorTest.java
@@ -1,22 +1,46 @@
 package com.appsdeveloperblog;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Test Math operations in Calculator class")
 class CalculatorTest {
 
+    Calculator calculator;
+
+    @BeforeAll
+    static void setup() {
+        System.out.println("Executing @BeforeAll method");
+    }
+
+    @AfterAll
+    static void cleanup() {
+        System.out.println("Executing @AfterAll method");
+    }
+
+    @BeforeEach
+    void beforeEachTestMethod() {
+        System.out.println("Executing @BeforeEach method");
+        calculator = new Calculator();
+    }
+
+    @AfterEach
+    void afterEachTestMethod() {
+        System.out.println("Executing @AfterEach method");
+    }
+
     // test<System Under Test>_<Condition or State Change>_<Expected Result>
 
     @DisplayName("Test 4/2 = 2")
     @Test
     void testIntegerDivision_WhenFourIsDividedByTwo_ShouldReturnTwo() {
+
+        System.out.println("Running Test 4/2 = 2");
+
         // AAA
 
         // Arrange or Given
-        Calculator calculator = new Calculator();
         int dividend = 4;
         int divisor = 2;
         int expectedResult = 2;
@@ -31,13 +55,18 @@ class CalculatorTest {
     @DisplayName("Division by zero")
     @Test
     void testIntegerDivision_WhenDividendIsDividedByZero_ShouldThrowArithmeticException() {
+
+        System.out.println("Running Division by zero");
+
         fail("Not implemented yet.");
     }
 
     @DisplayName("Test 33-1=32")
     @Test
     void integerSubtraction() {
-        Calculator calculator = new Calculator();
+
+        System.out.println("Running Test 33-1=32");
+
         int minuend = 33;
         int subtrahend = 1;
         int expectedResult = 32;
