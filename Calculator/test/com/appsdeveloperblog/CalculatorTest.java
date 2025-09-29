e4bb9df11e7b2e581577ff46d963973f57abd2ee
Szabolcs József Péter <Szabolcs-Jozsef.Peter@t-systems.com>
2025-09-29 11:03:11 +0200

    Naming Unit Tests

--- Calculator/test/com/appsdeveloperblog/CalculatorTest.java
+++ Calculator/test/com/appsdeveloperblog/CalculatorTest.java
@@ -6,11 +6,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    // test<System Under Test>_<Condition or State Change>_<Expected Result>
+
     @Test
-    void integerDivision() {
+    void testIntegerDivision_WhenFourIsDividedByTwo_ShouldReturnTwo() {
         Calculator calculator = new Calculator();
         int result = calculator.integerDivision(4, 2);
         assertEquals(2, result, "4/2 did not produce 2");
+    }
+
+    @Test
+    void testIntegerDivision_WhenDividendIsDividedByZero_ShouldThrowArithmeticException() {
+
     }
 
     @Test
