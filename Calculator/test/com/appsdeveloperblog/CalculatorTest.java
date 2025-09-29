34b925613ef83d97cd9a05bc1940b9e936e6f021
Szabolcs József Péter <Szabolcs-Jozsef.Peter@t-systems.com>
2025-09-29 10:02:29 +0200

    Subtraction test added

--- Calculator/test/com/appsdeveloperblog/CalculatorTest.java
+++ Calculator/test/com/appsdeveloperblog/CalculatorTest.java
@@ -12,4 +12,11 @@ class CalculatorTest {
         int result = calculator.integerDivision(4, 2);
         assertEquals(2, result, "4/2 did not produce 2");
     }
+
+    @Test
+    void integerSubtraction() {
+        Calculator calculator = new Calculator();
+        int result = calculator.integerSubtraction(33, 1);
+        assertEquals(32, result);
+    }
 }
