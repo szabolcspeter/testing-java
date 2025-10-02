d455ee350cfd82a0ea0bcab309a793c2dbc6a137
Szabolcs József Péter <Szabolcs-Jozsef.Peter@t-systems.com>
2025-10-02 10:00:07 +0200

    @ParameterizedTest + @ValueSource annotation

--- Calculator/test/com/appsdeveloperblog/CalculatorTest.java
+++ Calculator/test/com/appsdeveloperblog/CalculatorTest.java
@@ -2,10 +2,7 @@ package com.appsdeveloperblog;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.*;
 
 import java.util.stream.Stream;
 
@@ -79,6 +76,14 @@ class CalculatorTest {
         // Assert
         assertEquals(expectedExceptionMessage, actualException.getMessage(),
                 "Unexpected exception message");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"John", "Kate", "Alice"})
+    void valueSourceDemonstration(String firstName) {
+        System.out.println(firstName);
+
+        assertNotNull(firstName);
     }
 
     @DisplayName("Test integer subtraction [minuend, subtrahend, expectedResult]")
