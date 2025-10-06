92b02069089d618b9510ca8b06697c8c428666b2
Szabolcs József Péter <Szabolcs-Jozsef.Peter@t-systems.com>
2025-10-06 08:42:53 +0200

    Methods Order - Order by name

---
+++ Calculator/test/com/appsdeveloperblog/MethodsOrderedByNameTest.java
@@ -0,0 +1,29 @@
+package com.appsdeveloperblog;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class MethodsOrderedByNameTest {
+
+    @Test
+    void testD() {
+        System.out.println("Running test D");
+    }
+
+    @Test
+    void testA() {
+        System.out.println("Running test A");
+    }
+
+    @Test
+    void testC() {
+        System.out.println("Running test C");
+    }
+
+    @Test
+    void testB() {
+        System.out.println("Running test B");
+    }
+}
