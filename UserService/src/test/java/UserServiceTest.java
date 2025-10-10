5f92ea7dc3abe83dedd5508f74da1dbe008e3fbb
Szabolcs József Péter <Szabolcs-Jozsef.Peter@t-systems.com>
2025-10-10 15:10:19 +0200

    TDD: New project, Class, Method

---
+++ UserService/src/test/java/UserServiceTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+public class UserServiceTest {
+
+    @Test
+    void testCreateUser_whenUserDetailsProvided_returnsUserObject() {
+
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
