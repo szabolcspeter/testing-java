8dcbec3d1027a5a535afdbe22e06c5804cac09af
Szabolcs Peter <szabi.peter@gmail.com>
2025-10-19 13:31:41 +0200

    RequestBuilder - Building and Performing HTTP Request

--- UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerWebLayerTest.java
+++ UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerWebLayerTest.java
@@ -1,10 +1,63 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
+import com.appsdeveloperblog.tutorials.junit.ui.request.UserDetailsRequestModel;
+import com.appsdeveloperblog.tutorials.junit.ui.response.UserRest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @WebMvcTest(controllers = UsersController.class, excludeAutoConfiguration = {SecurityAutoConfiguration.class})
 // @AutoConfigureMockMvc(addFilters = false)
 public class UsersControllerWebLayerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("User can be created")
+    void testCreateUser_whenValidUserDetailsProvided_returnsCreatedUserDetails() throws Exception {
+
+        // Arrange
+        UserDetailsRequestModel userDetailsRequestModel = new UserDetailsRequestModel();
+        userDetailsRequestModel.setFirstName("Szabi");
+        userDetailsRequestModel.setLastName("Peter");
+        userDetailsRequestModel.setEmail("email");
+        userDetailsRequestModel.setPassword("12345678");
+        userDetailsRequestModel.setRepeatPassword("12345678");
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(userDetailsRequestModel));
+
+        // Act
+        MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+        String responseBodyAsString = mvcResult.getResponse().getContentAsString();
+        UserRest createdUser = new ObjectMapper()
+                .readValue(responseBodyAsString, UserRest.class);
+
+        // Assert
+        Assertions.assertEquals(userDetailsRequestModel.getFirstName(),
+                createdUser.getFirstName(), "The returned user first name is most likely incorrect");
+
+        Assertions.assertEquals(userDetailsRequestModel.getLastName(),
+                createdUser.getLastName(), "The returned user last name is incorrect");
+
+        Assertions.assertEquals(userDetailsRequestModel.getEmail(),
+                createdUser.getEmail(), "The returned user email is incorrect");
+
+        Assertions.assertFalse(createdUser.getUserId().isEmpty(), "userId should not be empty");
+    }
 }
