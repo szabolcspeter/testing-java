33b6737a25f5bf783e1cfeeff77aa2804224deeb
Szabolcs Peter <szabi.peter@gmail.com>
2025-10-21 10:20:04 +0200

    @SpringBootTest: Defined Port Number

--- UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
+++ UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -1,7 +1,13 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
+        properties = {"server.port=8081",
+        "hostname=fake"})
 public class UsersControllerIntegrationTest {
+
+    @Test
+    void contextLoads() {}
 }
