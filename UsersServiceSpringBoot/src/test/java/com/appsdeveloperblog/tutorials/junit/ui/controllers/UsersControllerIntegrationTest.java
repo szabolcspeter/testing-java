6e71c16153968ecbcb254197dbb6ee5c9df64294
Szabolcs Peter <szabi.peter@gmail.com>
2025-10-21 10:52:09 +0200

    @SpringBootTest: Random Port Number

--- UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
+++ UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerIntegrationTest.java
@@ -3,17 +3,24 @@ package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 //@TestPropertySource(locations = "/application-test.properties",
 //    properties = {"server.port=8081"})
-@TestPropertySource(locations = "/application-test.properties")
+//@TestPropertySource(locations = "/application-test.properties")
 public class UsersControllerIntegrationTest {
 
     @Value("${server.port}")
     private int serverPort;
 
+    @LocalServerPort
+    private int localServerPort;
+
     @Test
-    void contextLoads() {}
+    void contextLoads() {
+        System.out.println("server.port = " + serverPort);
+        System.out.println("localServerPort = " + localServerPort);
+    }
 }
