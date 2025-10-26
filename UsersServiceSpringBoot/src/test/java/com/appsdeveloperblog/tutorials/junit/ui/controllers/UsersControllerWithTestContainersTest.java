98dbb500cd7216a90b87eb3bd3740884b3f2eb34
Szabolcs Peter <szabi.peter@gmail.com>
2025-10-26 15:52:27 +0100

    @DynamicPropertySource annotation. Connecting to MySQL container

--- UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerWithTestContainersTest.java
+++ UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerWithTestContainersTest.java
@@ -1,6 +1,8 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -10,5 +12,16 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 public class UsersControllerWithTestContainersTest {
 
     @Container
-    private static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.4.0");
+    private static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.4.0")
+            .withDatabaseName("photo_app")
+            .withUsername("szabi")
+            .withPassword("szabi");
+
+    @DynamicPropertySource
+    private static void overrideProperties(DynamicPropertyRegistry registry) {
+
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+    }
 }
