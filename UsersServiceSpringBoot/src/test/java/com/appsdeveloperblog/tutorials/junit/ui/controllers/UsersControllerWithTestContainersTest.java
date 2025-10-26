89e96be9d120e05dc74ce73129be845d02a42191
Szabolcs Peter <szabi.peter@gmail.com>
2025-10-26 15:37:59 +0100

    @Container annotation: Creating a MySQL container

--- UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerWithTestContainersTest.java
+++ UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerWithTestContainersTest.java
@@ -1,9 +1,14 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
 public class UsersControllerWithTestContainersTest {
+
+    @Container
+    private static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.4.0");
 }
