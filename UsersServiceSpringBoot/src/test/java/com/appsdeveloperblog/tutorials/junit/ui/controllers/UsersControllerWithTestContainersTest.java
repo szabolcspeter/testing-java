b164650658c5cd3e4ba890d898330f0853055b08
Szabolcs Peter <szabi.peter@gmail.com>
2025-10-26 17:13:57 +0100

    Test if container is running, fix mysql dependency in pom.xml

--- UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerWithTestContainersTest.java
+++ UsersServiceSpringBoot/src/test/java/com/appsdeveloperblog/tutorials/junit/ui/controllers/UsersControllerWithTestContainersTest.java
@@ -1,5 +1,8 @@
 package com.appsdeveloperblog.tutorials.junit.ui.controllers;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -23,5 +26,16 @@ public class UsersControllerWithTestContainersTest {
         registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
         registry.add("spring.datasource.username", mySQLContainer::getUsername);
         registry.add("spring.datasource.password", mySQLContainer::getPassword);
+    }
+
+    @Test
+    @DisplayName("The MySQL container is created and is running")
+    void testContainerIsRunning() {
+
+        Assertions.assertTrue(mySQLContainer.isCreated(),
+                "MySQL container has not been created");
+
+        Assertions.assertTrue(mySQLContainer.isRunning(),
+                "MySQL container is not running");
     }
 }
