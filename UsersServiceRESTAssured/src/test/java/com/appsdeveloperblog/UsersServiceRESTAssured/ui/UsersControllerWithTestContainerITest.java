4e8270df74f112739314ac25622e776fcd4edcfa
Szabolcs Peter <szabi.peter@gmail.com>
2025-11-05 15:55:57 +0100

    given() - Setting up HTTP Headers

--- UsersServiceRESTAssured/src/test/java/com/appsdeveloperblog/UsersServiceRESTAssured/ui/UsersControllerWithTestContainerITest.java
+++ UsersServiceRESTAssured/src/test/java/com/appsdeveloperblog/UsersServiceRESTAssured/ui/UsersControllerWithTestContainerITest.java
@@ -1,5 +1,6 @@
 package com.appsdeveloperblog.UsersServiceRESTAssured.ui;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -38,6 +39,8 @@ public class UsersControllerWithTestContainerITest {
         // Act
         // *** It uses Fluent API ***
         given() // setup HTTP details
+                .contentType(ContentType.JSON) // longer syntax  .header("Content-Type", "application/json")
+                .accept(ContentType.JSON) // longer syntax .header("Accept", "application/json")
         .when() // used to specify HTTP method and API endpoint that we want to call
         .then(); // we verify HTTP response
         // Assert
